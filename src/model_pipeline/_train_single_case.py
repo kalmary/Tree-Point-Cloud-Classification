25084ff9334f753cef91254600aa56adadb1e5b9
Jakub <jakub.szota@itwl.pl>
2025-12-15 08:13:35 +0100

    _train_signle_case updated

--- src/model_pipeline/_train_single_case.py
+++ src/model_pipeline/_train_single_case.py
@@ -37,17 +37,24 @@ sys.path.append(nerual_net_dir)
 def train_model(training_dict: dict) -> Union[Generator[tuple[nn.Module, dict], None, None],
                                                 Generator[tuple[None, dict], None, None]]:      
     
+    device_gpu = torch.device('cuda')
+    device_cpu = torch.device('cpu')
+
+    device_loader = device_gpu
+    device_loss = device_gpu
+
+
     train_dataset = Dataset(path_dir = training_dict['data_path_train'],
                                  resolution_xy = training_dict['input_dim'],
                                  batch_size = training_dict['batch_size'],
                                  shuffle = True,
-                                 device = torch.device('cpu'))
+                                 device = device_loader)
     
     val_dataset = Dataset(path_dir = training_dict['data_path_val'],
                                resolution_xy=training_dict['input_dim'],
                                batch_size = training_dict['batch_size'],
                                shuffle = False,
-                               device = torch.device('cpu'))
+                               device = device_loader)
     
     trainLoader = DataLoader(train_dataset,
                              batch_size=None,
@@ -61,7 +68,7 @@ def train_model(training_dict: dict) -> Union[Generator[tuple[nn.Module, dict], 
     
     total_t = get_dataset_len(trainLoader)
     total_v = get_dataset_len(valLoader)
-
+# START
     weights_t = calculate_class_weights(trainLoader,
                                         training_dict['num_classes'],
                                         total = total_t,
@@ -70,17 +77,36 @@ def train_model(training_dict: dict) -> Union[Generator[tuple[nn.Module, dict], 
     weights_v = calculate_class_weights(valLoader,
                                         training_dict['num_classes'],
                                         total = total_v,
-                                        device=training_dict['device'],
+                                        device=training_dict['device'], ##TODO Changed
                                         verbose=False)
+    train_dataset = Dataset(path_dir = training_dict['data_path_train'],
+                                resolution_xy = training_dict['input_dim'],
+                                batch_size = training_dict['batch_size'],
+                                shuffle = True,
+                                weights=weights_t,
+                                device = torch.device('cpu'))
     
+    trainLoader = DataLoader(train_dataset,
+                             batch_size=None,
+                             num_workers = 15,          
+                             pin_memory=True)
+    
+    total_t = get_dataset_len(trainLoader)
+
+    weights_t = calculate_class_weights(trainLoader,
+                                    training_dict['num_classes'],
+                                    total = total_t,
+                                    device=training_dict['device'],
+                                    verbose=False)
+#STOP
     try:
         model = CNN2D_Residual(training_dict['num_classes'], config_data=training_dict['model_config']).to(training_dict['device'])
     except Exception as e:
         print(f"Error initializing model: {e}")
         yield None, {}
 
-    criterion_t = CrossEntropyLoss(weight=weights_t.cpu()).cpu()
-    criterion_v = CrossEntropyLoss(weight=weights_v.cpu()).cpu()
+    criterion_t = FocalLoss_CLASS(alpha= weights_t, gamma=training_dict['focal_loss_gamma']).cpu()  ##TODO Changed  
+    criterion_v = FocalLoss_CLASS(alpha= weights_v, gamma=training_dict['focal_loss_gamma']).cpu()
 
     optimizer = optim.Adam(model.parameters(), lr = training_dict['learning_rate'])
 
@@ -187,7 +213,9 @@ def train_model(training_dict: dict) -> Union[Generator[tuple[nn.Module, dict], 
 
                         loss_v = criterion_v(outputs, batch_y)
 
-                        accuracy_v = calculate_accuracy_weighted(outputs, batch_y)
+                        accuracy_v = calculate_accuracy_weighted(outputs, 
+                                                                 batch_y, 
+                                                                 num_classes=training_dict['num_classes']) ##TODO Changed 
 
                         epoch_loss_v += loss_v.item() * batch_y.size(0)
                         epoch_accuracy_v += accuracy_v * batch_y.size(0)
@@ -204,7 +232,6 @@ def train_model(training_dict: dict) -> Union[Generator[tuple[nn.Module, dict], 
                 loss_v_hist.append(avg_loss_v)
                 acc_v_hist.append(avg_accuracy_v)
 
-                early_stopping.check_early_stop(loss_v_hist[-1])
 
                 hist_dict = wrap_hist(acc_hist, loss_hist, acc_v_hist, loss_v_hist)
 
