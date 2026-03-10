c7ef45ccbf8f1333065b9aa29b14f77133a085e9
michal-siniarski <siniarskim@gmail.com>
2026-03-10 12:35:19 +0100

    add new models

--- src/TreeClassifier.py
+++ src/TreeClassifier.py
@@ -4,15 +4,16 @@ import torch
 import torch.nn as nn
 import pathlib as pth
 
-from src.model_pipeline.model import CNN2D_Residual
-from src.utils import load_model, load_json
-from src.utils.data_augmentation import gaussian_blur, cloud2sideViews_torch
+from final_files.model_test import EfficientNetClassifier
+from final_files.ResNet import ResNetTreeClassifier
+from utils import load_model, load_json
+from utils.data_augmentation import gaussian_blur, cloud2sideViews_torch
 
 
 class TreeClassifier:
     def __init__(self,
                  model_name: str = None,
-                 config_dir: Union[str, pth.Path] = "./final_files",
+                 config_dir: Union[str, pth.Path] = "src/final_files",
                  device: torch.device = torch.device('cpu')):
         self.model_name = model_name + '.pt'
         if isinstance(device, str):
@@ -26,15 +27,17 @@ class TreeClassifier:
 
         config_path = pth.Path(config_dir).joinpath(self.model_name.replace('.pt', '_config.json'))
         config_dict = load_json(config_path)
-        self._model_config: dict = config_dict['model_config']
-        self.voxel_size_small: float = self.model_config['max_voxel_dim']
+        # self._model_config: dict = config_dict['model_config']
+        self._model_config: dict = config_dict
+
+        
 
         return config_dict
 
-    def _load_segmModel(self, model_dir: Union[pth.Path, str] = "./final_files") -> nn.Module:
+    def _load_model(self, model_dir: Union[pth.Path, str] = "./final_files") -> nn.Module:
 
         path2model = pth.Path(model_dir).joinpath(self.model_name)
-        model = CNN2D_Residual.from_config_file(self._model_config, self._model_config['num_classes'])
+        model = ResNetTreeClassifier(self._model_config['num_channels'], self._model_config['num_classes'])
         self._model: nn.Module = load_model(file_path=path2model,
                                             model=model,
                                             device=self.device)
@@ -44,9 +47,9 @@ class TreeClassifier:
         cloud  = torch.asarray(cloud)
         imgs = cloud2sideViews_torch(cloud,
                                     resolution_xy=350)
-        imgs = gaussian_blur(imgs,
-                             kernel_size=(3, 3),
-                             sigma = 0.6)
+        # imgs = gaussian_blur(imgs,
+        #                      kernel_size=(3, 3),
+        #                      sigma = 0.6)
         if imgs.ndim == 3:
             imgs = imgs.unsqueeze(0)
         output = self._model(imgs)
@@ -55,3 +58,9 @@ class TreeClassifier:
 
         labels = labels.flatten()
         return labels
+
+def main():
+    TreeClassifier(model_name="ResNetTreeV0_61", device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+
+if __name__ == '__main__':
+    main()
